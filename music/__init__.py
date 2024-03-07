fc444bfbd385379d3375d2a3136842a5304c2289
Faris Chebib <faris@theluckybead.com>
2024-03-06 18:29:46 -0700

    Add ConfigurableLocalOutputNotebookIOManager and update asset parameters in music assets
    Introduced ConfigurableLocalOutputNotebookIOManager in __init__.py to manage local outputs. Also, updated the group name, description, and method name in artists.py, transitioning from 'space_insights' to 'artists'. This shift aligns with the new focus of the music assets on 'artists' and their concerts instead of 'space_insights'.

--- music/__init__.py
+++ music/__init__.py
@@ -1,6 +1,7 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules, file_relative_path
 
 from dagster_duckdb import DuckDBResource
+from dagstermill import ConfigurableLocalOutputNotebookIOManager
 
 from . import assets
 
@@ -10,5 +11,8 @@ defs = Definitions(
     assets=all_assets,
     resources={
         "motherduck": DuckDBResource(database="md:"),
+        "nb_io_manager": ConfigurableLocalOutputNotebookIOManager(
+            base_dir=file_relative_path(__file__, "notebooks/outputs")
+        ),
     }
 )
--- music/assets/artists.py
+++ music/assets/artists.py
@@ -10,27 +10,27 @@ from dagstermill import define_dagstermill_asset
 
 
 @asset(
-    group_name="seer_sync_space_insights",
-    description="Cleanup old, deleted spaces as partitions",
+    group_name="artists",
+    description="Artists asset",
     compute_kind="pandas"
 )
-def clean_inactive_space_partitions(context: AssetExecutionContext):
+def fetch_concerts(context: AssetExecutionContext):
     context.log.info("Fetching concerts for today")
     df = pd.DataFrame()
     return df
 
 
 # # Uncomment to debug, note this will slow down downstream jobs when enabled
-space_insights_playground = define_dagstermill_asset(
-    name="space_insights_playground",
+artists_playground = define_dagstermill_asset(
+    name="artists_playground",
     notebook_path=file_relative_path(
         __file__,
         "../notebooks/inputs/music_events.ipynb"
     ),
-    group_name="seer_sync_space_insights",
-    description="An enriched, space-by-space exploration of documents in a space.",
+    group_name="artists",
+    description="An enriched, artist playground.",
     ins={
-        "space_df": AssetIn("space_df"),
+        "fetch_concerts": AssetIn("fetch_concerts"),
 
     },
     io_manager_key="nb_io_manager"
